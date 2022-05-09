d8a2d6bde27fe2f4b89e40a4526ff13905b6e5b4
kamikat1 <88120995+kamikat1@users.noreply.github.com>
2022-05-09 01:23:38 -0300

    Create dois.c

---
+++ dois.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float vet[5]={1, 2, 3, 4, 5};
+    printf("%f ", sqrt((pow(vet[0], 2)) + (pow(vet[1] , 2)) + (pow(vet[2] , 2) + pow(vet[3] , 2)) + pow(vet[4] , 2) + pow(vet[5] , 2)));
+}
+
